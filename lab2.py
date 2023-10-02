1f56e8bae10f5d6119eec9fee7f579472a275297
RabbitsAreUs <146285648+RabbitsAreUs@users.noreply.github.com>
2023-10-02 22:36:31 +0000

    Alternate Lab2

--- lab2.py
+++ lab2.py
@@ -1,6 +1,24 @@
 print('Hello lab2.py')
-for x in range(5):
-  print('Hello')
+
+for x in range(1):
+  print(' * ', end="")
+
+print("")
+
+for x in range(2):
+  print(' * ', end="")
+
+print("")
 
 for x in range(3):
-  print('*')
+  print(' * ', end="")
+
+print("")
+
+for x in range(4):
+  print(' * ', end="")
+
+print("")
+
+for x in range(5):
+  print(' * ', end="")
